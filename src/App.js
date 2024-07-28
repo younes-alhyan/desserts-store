6b5beb3bb34b13f01fb5f543270f1240e83e9f4e
itachi <gameryounes555@gmail.com>
2024-07-28 16:53:49 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Desserts from './Desserts/Desserts';
+import Cart from './Cart/Cart';
+import Order from './Cart/confirmed-order/Order';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch('/data.json'); // Adjust the path if necessary
+        if (!res.ok) throw new Error('Network response was not ok');
+        const result = await res.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const updateCart = (item) => {
+    setCartItems((prevItems) => {
+      const existingItemIndex = prevItems.findIndex(i => i.name === item.name);
+      if (existingItemIndex !== -1) {
+        if (item.quantity === 0) {
+          return prevItems.filter(i => i.name !== item.name);
+        }
+        // Update existing item
+        const updatedItems = [...prevItems];
+        updatedItems[existingItemIndex] = item;
+        return updatedItems;
+      } else {
+        // Add new item
+        return [...prevItems, item];
+      }
+    });
+  };
+
+  const removeItem = (item) => {
+    setCartItems((prevItems) => {
+      // Filter out the item to be removed
+      return prevItems.filter(i => i.name !== item.name);
+    });
+  };
+
+  const clearCart = () => {
+    cartItems.forEach((item) => {
+      removeItem(item);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <Desserts items={data} addToCart={updateCart} cart={cartItems} />
+        <Cart items={cartItems} addToCart={updateCart} removeFromCart={removeItem} togglePopup={togglePopup} />
+      </div>
+      <Order cart={cartItems} show={showPopup} togglePopup={togglePopup} clearCart={clearCart}/>
     </div>
   );
 }
